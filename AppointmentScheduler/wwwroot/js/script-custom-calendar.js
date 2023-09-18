82bc8937a76058209e0db5d7b794fc6ee19266ef
ShakeelAbbasKhan <shakeelabbaskhan4455@gmail.com>
2023-09-18 10:16:46 +0500

    changes in calender.js

--- AppointmentScheduler/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduler/wwwroot/js/script-custom-calendar.js
@@ -25,40 +25,9 @@ function InitializeCalendar() {
                 editable: false,
                 select: function (event) {
                     onShowModal(event, null);
-                },
-                eventDisplay: 'block',
-                events: function (fetchInfo, successCallback, failureCallback) {
-                    $.ajax({
-                        url: routeURL + '/api/Appointment/GetCalendarData?doctorId=' + $("#doctorId").val(),
-                        type: 'GET',
-                        dataType: 'JSON',
-                        success: function (response) {
-                            var events = [];
-                            if (response.status === 1) {
-                                $.each(response.dataenum, function (i, data) {
-                                    events.push({
-                                        title: data.title,
-                                        description: data.description,
-                                        start: data.startDate,
-                                        end: data.endDate,
-                                        backgroundColor: data.isDoctorApproved ? "#28a745" : "#dc3545",
-                                        borderColor: "#162466",
-                                        textColor: "white",
-                                        id: data.id
-                                    });
-                                })
-                            }
-                            successCallback(events);
-                        },
-                        error: function (xhr) {
-                            $.notify("Error", "error");
-                        }
-                    });
-                },
-                eventClick: function (info) {
-                    getEventDetailsByEventId(info.event);
-                }
-            });
+
+                } 
+                });
             calendar.render();
         }
 
@@ -69,47 +38,11 @@ function InitializeCalendar() {
 
 }
 
-
 function onShowModal(obj, isEventDetail) {
-    if (isEventDetail != null) {
-
-        $("#title").val(obj.title);
-        $("#description").val(obj.description);
-        $("#appointmentDate").val(obj.startDate);
-        $("#duration").val(obj.duration);
-        $("#doctorId").val(obj.doctorId);
-        $("#patientId").val(obj.patientId);
-        $("#id").val(obj.id);
-        $("#lblPatientName").html(obj.patientName);
-        $("#lblDoctorName").html(obj.doctorName);
-        if (obj.isDoctorApproved) {
-            $("#lblStatus").html('Approved');
-            $("#btnConfirm").addClass("d-none");
-            $("#btnSubmit").addClass("d-none");
-        }
-        else {
-            $("#lblStatus").html('Pending');
-            $("#btnConfirm").removeClass("d-none");
-            $("#btnSubmit").removeClass("d-none");
-        }
-        $("#btnDelete").removeClass("d-none");
-    }
-    else {
-        $("#appointmentDate").val(obj.startStr + " " + new moment().format("hh:mm A"));
-        $("#id").val(0);
-        $("#btnDelete").addClass("d-none");
-        $("#btnSubmit").removeClass("d-none");
-    }
     $("#appointmentInput").modal("show");
 }
 
 function onCloseModal() {
-    $("#apointmentForm")[0].reset();
-    $("#id").val(0);
-    $("#title").val('');
-    $("#description").val('');
-    $("#appointmentDate").val('');
-
     $("#appointmentInput").modal("hide");
 }
 
@@ -132,7 +65,6 @@ function onSubmitForm() {
             contentType: 'application/json',
             success: function (response) {
                 if (response.status === 1 || response.status === 2) {
-                    calendar.refetchEvents();
                     $.notify(response.message, "success");
                     onCloseModal();
                 }
@@ -167,74 +99,4 @@ function checkValidation() {
 
     return isValid;
 
-}
-
-function getEventDetailsByEventId(info) {
-    $.ajax({
-        url: routeURL + '/api/Appointment/GetCalendarDataById/' + info.id,
-        type: 'GET',
-        dataType: 'JSON',
-        success: function (response) {
-
-            if (response.status === 1 && response.dataenum !== undefined) {
-                onShowModal(response.dataenum, true);
-            }
-            successCallback(events);
-        },
-        error: function (xhr) {
-            $.notify("Error", "error");
-        }
-    });
-}
-
-function onDoctorChange() {
-    calendar.refetchEvents();
-}
-
-function onDeleteAppointment() {
-    var id = parseInt($("#id").val());
-    $.ajax({
-        url: routeURL + '/api/Appointment/DeleteAppoinment/' + id,
-        type: 'GET',
-        dataType: 'JSON',
-        success: function (response) {
-
-            if (response.status === 1) {
-                $.notify(response.message, "success");
-                calendar.refetchEvents();
-                onCloseModal();
-            }
-            else {
-
-                $.notify(response.message, "error");
-            }
-        },
-        error: function (xhr) {
-            $.notify("Error", "error");
-        }
-    });
-}
-
-function onConfirm() {
-    var id = parseInt($("#id").val());
-    $.ajax({
-        url: routeURL + '/api/Appointment/ConfirmEvent/' + id,
-        type: 'GET',
-        dataType: 'JSON',
-        success: function (response) {
-
-            if (response.status === 1) {
-                $.notify(response.message, "success");
-                calendar.refetchEvents();
-                onCloseModal();
-            }
-            else {
-
-                $.notify(response.message, "error");
-            }
-        },
-        error: function (xhr) {
-            $.notify("Error", "error");
-        }
-    });
 }
